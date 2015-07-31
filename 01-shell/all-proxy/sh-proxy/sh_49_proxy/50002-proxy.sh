c484b503c33dabc79ed92734bd815909309f4f64
franklyu <zhenxin125114@163.com>
2015-07-31 18:02:03 +0800

    add pac.txt

--- 01-shell/all-proxy/sh-proxy/sh_49_proxy/50002-proxy.sh
+++ 01-shell/all-proxy/sh-proxy/sh_49_proxy/50002-proxy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-proxyIP="54.169.155.70"
+#proxyIP="54.169.155.70"
+proxyIP="54.169.100.247"
 proxyName="SG-proxy-2-SG2_${proxyIP}"
 ssh_ops="-oConnectTimeout=30 -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no"
 
 local_port="50002"
-remote_port="3400"
+#remote_port="6082"
+remote_port="3128"
 
 echo "Start Proxy  ${proxyName}"
 
-autossh  -M 60002 ${ssh_ops} -Nnf -L 0.0.0.0:${local_port}:127.0.0.1:${remote_port} ec2-user@${proxyIP} -p38022 -i /root/.ssh/test_rsa
+autossh  -M 60002 ${ssh_ops} -Nnf -L 0.0.0.0:${local_port}:127.0.0.1:${remote_port} ec2-user@${proxyIP} -p22 -i /root/.ssh/test_rsa
 if [ $? = 0 ];then
 	echo "Start port ${local_port} to ${proxyName}:${remote_port} OK ^-^"
 fi
