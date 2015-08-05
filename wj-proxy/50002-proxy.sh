3635cb749e06dc5eadcc26ce9a03f481a01a03ba
yu_lin <2629043232@qq.com>
2015-08-05 12:19:00 +0800

    add SG_proxy all files for WJ

--- wj-proxy/50002-proxy.sh
+++ wj-proxy/50002-proxy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #proxyIP="54.169.155.70"
-proxyIP="54.169.100.247"
-proxyName="SG-proxy-2-SG2_${proxyIP}"
+#proxyIP="54.169.100.247"
+proxyIP="52.74.236.200"
+proxyName="SG-0-proxy_${proxyIP}"
 ssh_ops="-oConnectTimeout=30 -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no"
 
 local_port="50002"
@@ -12,7 +13,7 @@ remote_port="3128"
 echo "Start Proxy  ${proxyName}"
 
 #autossh  -M 60002 ${ssh_ops} -Nnf -L 0.0.0.0:${local_port}:127.0.0.1:${remote_port} ec2-user@${proxyIP} -p22 -i /root/.ssh/test_rsa
-autossh  -M 60002 ${ssh_ops} -Nnf -L 0.0.0.0:${local_port}:127.0.0.1:${remote_port} ec2-user@${proxyIP} -p22 -i /home/ec2-user/.ssh/id_rsa
+autossh  -M 60002 ${ssh_ops} -Nnf -L 0.0.0.0:${local_port}:127.0.0.1:${remote_port} ec2-user@${proxyIP} -p58022 -i /root/.ssh/sg_proxy.pem
 if [ $? = 0 ];then
 	echo "Start port ${local_port} to ${proxyName}:${remote_port} OK ^-^"
 fi
