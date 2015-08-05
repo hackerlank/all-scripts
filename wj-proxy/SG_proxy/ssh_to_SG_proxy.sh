1721490090fd8864c47cb2ab6f90a8d1f94edce6
yu_lin <2629043232@qq.com>
2015-08-05 13:06:34 +0800

    add proxy three IPs

--- wj-proxy/SG_proxy/ssh_to_SG_proxy.sh
+++ wj-proxy/SG_proxy/ssh_to_SG_proxy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash 
 
 ssh -p58022 ec2-user@52.74.236.200 -i /root/.ssh/sg_proxy.pem
+ssh -p58022 ec2-user@52.76.11.171 -i /root/.ssh/sg_proxy.pem
+ssh -p58022 ec2-user@52.74.98.63 -i /root/.ssh/sg_proxy.pem
+ssh -p58022 ec2-user@52.76.15.252 -i /root/.ssh/sg_proxy.pem
