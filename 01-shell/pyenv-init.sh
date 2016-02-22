505878f9ac7e7184e4baec93cc826f561b9cf42f
franklyu <zhenxin125114@163.com>
2016-02-22 03:11:29 +0000

    add pyenv-init.sh for pytenv init

---
+++ 01-shell/pyenv-init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+cd /root
+
+git clone https://github.com/yyuu/pyenv.git .pyenv
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc 
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+exec $SHELL
