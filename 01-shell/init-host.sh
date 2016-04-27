b513221f8db79cda3134f53f729a9058d7e769fe
franklyu <zhenxin125114@163.com>
2016-04-27 02:08:10 +0000

    update init-host.sh

--- 01-shell/init-host.sh
+++ 01-shell/init-host.sh
@@ -8,6 +8,8 @@ chkconfig ip6tables off
 chkconfig iptables off
 chkconfig --list|grep ip
 
+yum install -y vim 
+
 sed -i 's/DIR\ 01;34/DIR\ 38;5;27/g' /etc/DIR_COLORS
 sed -i 's/EXEC\ 01;32/EXEC\ 38;5;34/g' /etc/DIR_COLORS
 sed -i '$a colorscheme desert' /etc/vimrc
