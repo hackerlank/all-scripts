9932ab68e94f40f5e5b49507ba10e6dc44ac968e
franklyu <zhenxin125114@163.com>
2016-04-27 02:17:02 +0000

    update init-host.sh

--- 01-shell/init-host.sh
+++ 01-shell/init-host.sh
@@ -8,11 +8,14 @@ chkconfig ip6tables off
 chkconfig iptables off
 chkconfig --list|grep ip
 
-yum install -y vim 
+yum install -y vim  
 
 sed -i 's/DIR\ 01;34/DIR\ 38;5;27/g' /etc/DIR_COLORS
 sed -i 's/EXEC\ 01;32/EXEC\ 38;5;34/g' /etc/DIR_COLORS
 sed -i '$a colorscheme desert' /etc/vimrc
 
+if [ ! -e /usr/bin/rsync ];then
+    yum install -y rsync
+fi
 # wget -O git-flow-0.4.1_git201202141310-1.9.noarch.rpm ftp://ftp.pbone.net/mirror/ftp5.gwdg.de/pub/opensuse/repositories/devel:/tools:/scm/openSUSE_Factory/noarch/git-flow-0.4.1_git201202141310-1.9.noarch.rpm
 # wget -O  bash-completion-20060301-11.noarch.rpm ftp://ftp.pbone.net/mirror/atrpms.net/el6-x86_64/atrpms/stable/bash-completion-20060301-11.noarch.rpm
