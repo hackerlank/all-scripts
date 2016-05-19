681cd1eb31ebdabbad2fae8cd018397b8ddffe5d
yu_lin <yu_lin@gjzq.com.cn>
2016-05-19 17:10:04 +0800

    update kafka-command-test.sh for add kafka-configs.sh

--- Linux-Servers/Kafka/kafka-command-test.sh
+++ Linux-Servers/Kafka/kafka-command-test.sh
@@ -8,3 +8,7 @@ kafka-topics.sh --zookeeper 172.28.25.38:2181 --delete --topic myToic
 kafka-topics.sh --zookeeper 172.28.25.37:2181 --alter --topic trade3 --partitions 3
 
 kafka-topics.sh --zookeeper 172.28.25.38:2181 --describe
+
+kafka-configs.sh --zookeeper 172.28.25.38:2181 --alter --add-config 'producer_byte_rate=1024,consumer_byte_rate=2048' --entity-name trade3 --entity-type clients
+
+kafka-configs.sh --zookeeper 172.28.25.38:2181 --describe --entity-name trade3 --entity-type clients
